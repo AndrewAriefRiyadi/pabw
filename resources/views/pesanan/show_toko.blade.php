e0c462b21ae47bc022bb047ca0b838bdfc554400
AndrewAriefRiyadi <andrewariefr@gmail.com>
2024-05-13 22:31:09 +0800

    mau change ke anisya

--- resources/views/pesanan/show_toko.blade.php
+++ resources/views/pesanan/show_toko.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white  overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 flex flex-col bg-gray-300 text-gray-900 ">
-                    <h1 class=" font-bold"> PESANAN </h1>
+                    <h1 class=" font-bold"> PESANAN TOKO </h1>
                     @if(session('success'))
                         <div class="alert alert-success">
                             {{ session('success') }}
